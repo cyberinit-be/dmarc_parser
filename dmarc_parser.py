487b435ef9302483ffe61e3bbf9f3980069b2911
cyberinit-be <166705290+cyberinit-be@users.noreply.github.com>
2024-04-11 11:34:02 +0200

    Update dmarc_parser.py

--- dmarc_parser.py
+++ dmarc_parser.py
@@ -1,10 +1,6 @@
 import sys
 from defusedxml.ElementTree import parse as ET_parse
 import csv
-
-# Function to parse the XML report
-import os
-
 import os
 import datetime
 
@@ -12,15 +8,11 @@ def parse_xml(xml_file):
     tree = ET_parse(xml_file)
     root = tree.getroot()
     data = []
-
-    # Get the date range (begin and end) and convert epoch timestamps to "DD/MM/YYYY" format
     date_range = root.find('.//date_range')
     begin_timestamp = int(find_text(date_range, './/begin'))
     end_timestamp = int(find_text(date_range, './/end'))
     begin_date = datetime.datetime.fromtimestamp(begin_timestamp).strftime('%d/%m/%Y')
     end_date = datetime.datetime.fromtimestamp(end_timestamp).strftime('%d/%m/%Y')
-
-    # Get the filename (without the path)
     filename = os.path.basename(xml_file)
 
     for record in root.findall('.//record'):
@@ -60,19 +52,10 @@ def parse_xml(xml_file):
 
     return data
 
-
-
-
-
-
-
-
-
 def find_text(element, path, default=''):
     elem = element.find(path)
     return elem.text if elem is not None else default
 
-# Function to write data to a CSV file
 def write_to_csv(data, csv_file):
     keys = data[0].keys()
     with open(csv_file, 'w', newline='') as output_file:
@@ -80,7 +63,6 @@ def write_to_csv(data, csv_file):
         dict_writer.writeheader()
         dict_writer.writerows(data)
 
-# Main function
 def main():
     if len(sys.argv) < 3:
         print("Usage: python dmarc_parser.py output.csv input1.xml input2.xml ...")
