b78a706a895d2be01f8a7a54309e3c22d972cf18
CyberInIT <166705290+cyberinit-be@users.noreply.github.com>
2024-04-15 10:14:28 +0200

    Update dmarc_parser.py

--- dmarc_parser.py
+++ dmarc_parser.py
@@ -8,11 +8,13 @@ def parse_xml(xml_file):
     tree = ET_parse(xml_file)
     root = tree.getroot()
     data = []
+
     date_range = root.find('.//date_range')
     begin_timestamp = int(find_text(date_range, './/begin'))
     end_timestamp = int(find_text(date_range, './/end'))
     begin_date = datetime.datetime.fromtimestamp(begin_timestamp).strftime('%d/%m/%Y')
     end_date = datetime.datetime.fromtimestamp(end_timestamp).strftime('%d/%m/%Y')
+
     filename = os.path.basename(xml_file)
 
     for record in root.findall('.//record'):
@@ -35,17 +37,30 @@ def parse_xml(xml_file):
         dkim_auths = auth_results.findall('.//dkim')
         spf_auths = auth_results.findall('.//spf')
 
+        dkim_result = None
         if dkim_auths:
             dkim_auth = dkim_auths[0]
             row['dkim_domain'] = find_text(dkim_auth, './/domain')
             row['dkim_selector'] = find_text(dkim_auth, './/selector')
-            row['dkim_result'] = find_text(dkim_auth, './/result')
+            dkim_result = find_text(dkim_auth, './/result')
+        else:
+            row['dkim_domain'] = ''
+            row['dkim_selector'] = ''
+
+        if dkim_result is None:
+            dkim_result = row['dkim']
+
+        row['dkim_result'] = dkim_result
 
         if spf_auths:
             spf_auth = spf_auths[0]
             row['spf_domain'] = find_text(spf_auth, './/domain')
             row['spf_scope'] = find_text(spf_auth, './/scope', default='')
             row['spf_result'] = find_text(spf_auth, './/result')
+        else:
+            row['spf_domain'] = ''
+            row['spf_scope'] = ''
+            row['spf_result'] = row['spf']
 
         row['filename'] = filename
         data.append(row)
@@ -56,11 +71,27 @@ def find_text(element, path, default=''):
     elem = element.find(path)
     return elem.text if elem is not None else default
 
+def read_csv(csv_file):
+    data = []
+    try:
+        with open(csv_file, 'r', newline='') as input_file:
+            reader = csv.DictReader(input_file)
+            for row in reader:
+                data.append(row)
+    except FileNotFoundError:
+        pass
+    return data
+
+
 def write_to_csv(data, csv_file):
     keys = data[0].keys()
-    with open(csv_file, 'w', newline='') as output_file:
+    with open(csv_file, 'a', newline='') as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
-        dict_writer.writeheader()
+
+        # Write the header only if the file is empty or does not exist
+        if os.path.getsize(csv_file) == 0 or not os.path.exists(csv_file):
+            dict_writer.writeheader()
+
         dict_writer.writerows(data)
 
 def main():
